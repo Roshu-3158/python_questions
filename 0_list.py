2a2ff484a47b4971cfcc4351d72d1371a51f09f0
Roshu-3158 <roshandnikam3158@gmail.com>
2023-03-04 23:26:48 +0530

    add 4th program

--- 0_list.py
+++ 0_list.py
@@ -3,5 +3,6 @@
 1) Python program to print "Hello Python"
 2) Python program to do arithmetical operations
 3) Python program to find the area of a triangle
+4) Simple calculator in python using if else
 
 """
