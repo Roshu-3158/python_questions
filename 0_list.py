b7a57cbe12f0ee5982fb4f8998b1239c9075bebc
Roshu-3158 <roshandnikam3158@gmail.com>
2023-03-05 22:08:19 +0530

    eligible for vote or not

--- 0_list.py
+++ 0_list.py
@@ -4,5 +4,6 @@
 2) Python program to do arithmetical operations
 3) Python program to find the area of a triangle
 4) Simple calculator in python using if else
+5) eligible to vote or not
 
 """
---
+++ 5_five.py
@@ -0,0 +1,7 @@
+# you are eligible for vote or not 
+
+age = int (input("Enter your age? "))  
+if age>=18:  
+    print("You are eligible to vote !!");  
+else:  
+    print("Sorry! you have to wait !!");  
