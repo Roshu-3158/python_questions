5a6ef0eb582d1637e4faaa19a206127dfc8cead3
Roshu-3158 <roshandnikam3158@gmail.com>
2023-03-09 10:12:56 +0530

    short the program

--- 7_seven.py
+++ 7_seven.py
@@ -2,18 +2,21 @@
 
 print("Prime number or not")
 print("")
-a =int(input("Enter the number : "))
-r = 0
+a = int(input("Enter your number : "))
+r=0
 for i in range(2,a):
-    if(a%i==0):
-        r=1
+    if(a%2==0):
+        print(" Not a Prime number")
         break
     else:
-        r=0
+        print("Prime number")
         break
-if(r==0):
-    print("it is a prime number")
-else:
-    print("It is not a prime number")
+
+# if(r==0):
+#     print("Prime number")
+# else:
+#     print("not a prime number")
+
+
 
 
