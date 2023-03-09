c262b2c4f3c1f30ea0b2c102744c48fb57a28f01
Roshu-3158 <roshandnikam3158@gmail.com>
2023-03-09 11:09:50 +0530

    adding do you wants to continue massage.

--- 7_seven.py
+++ 7_seven.py
@@ -2,13 +2,16 @@
 
 print("Prime number or not")
 print("")
-a = int(input("Enter your number : "))
-for i in range(a):
-    if(a==1):
-        print("not prime not composite")
-    elif(a%2==0):
-        print("Not a Prime number")
-        break
-    else:
-        print("Prime number")
-        break
+k="y"
+while(k is not "n"):
+    a = int(input("Enter your number : "))
+    for i in range(a):
+        if(a==1):
+            print("not prime not composite")
+        elif(a%2==0):
+            print("Not a Prime number")
+            break
+        else:
+            print("Prime number")
+            break
+    k=input("do you wants to continue ? y/n :")
